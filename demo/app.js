080cf300c7abb91f6205e04559820e94791c9be1
Siddique Hameed <siddii+git@gmail.com>
2015-03-11 22:34:36 -0500

    first commit

--- demo/app.js
+++ demo/app.js
@@ -1,41 +1,20 @@
 angular.module('material-demo', ['ngMaterial'])
   .controller('AppCtrl', function($scope, $mdToast, $mdDialog) {
-    $scope.toastPosition = {
-      bottom: false,
-      top: true,
-      left: false,
-      right: true
-    };
-    $scope.getToastPosition = function() {
-      return Object.keys($scope.toastPosition)
-        .filter(function(pos) { return $scope.toastPosition[pos]; })
-        .join(' ');
-    };
     $scope.showCustomToast = function() {
       $mdToast.show({
         controller: 'ToastCtrl',
         templateUrl: 'toast-template.html',
-        hideDelay: 6000,
-        position: $scope.getToastPosition()
-      });
-    };
-    $scope.showActionToast = function() {
-      var toast = $mdToast.simple()
-        .content('Action Toast!')
-        .action('OK')
-        .highlightAction(false)
-        .position($scope.getToastPosition());
-      $mdToast.show(toast).then(function() {
-        alert('You clicked \'OK\'.');
+        hideDelay: 5000,
+        position: 'top right'
       });
     };
 
     $scope.showAlert = function(ev) {
       $mdDialog.show(
         $mdDialog.alert()
-          .title('FAB')
-          .content('Hey, I am a Floating Action Bar')
-          .ok('Got it!')
+          .title('FAB?')
+          .content('Yes, I am a Floating Action Button')
+          .ok('Go back')
           .targetEvent(ev)
       );
     };
